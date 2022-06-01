6027f26e80876e4fcd55364f8e68089a336ed5c0
brena-odwyer <odwyer.brena@gmail.com>
2022-06-01 19:17:04 -0300

    atualizando

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+function somar (num1, num2){
+	let soma = num1 + num2
+	return console.log(`A soma é ${soma}`)
+}
+
+function subtrair (num1, num2){
+	let subtracao = num1 - num2
+	return console.log(`A soma é ${subtracao}`)
+}
+
+function multiplicar (num1, num2){
+	let multiplicacao = num1 * num2
+	return console.log(`A soma é ${multiplicacao}`)
+}
+
+function dividir (num1, num2){
+	let divisao = num1 / num2
+	return console.log(`A soma é ${divisao}`)
+}
+
+somar(7,9)
+subtrair(10,2)
+multiplicar(2,4)
+dividir(8,2)
